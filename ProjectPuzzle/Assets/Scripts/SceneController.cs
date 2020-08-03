012ea72962d5879da5c81d9123252f4a4c60a8d7
Rynder1 <liam.ellis37@gmail.com>
2020-08-03 13:01:04 +1000

    MainMenu Changes
    Added added scenes and scripts to help main menu navigation

---
+++ ProjectPuzzle/Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void AddScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void CloseScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
