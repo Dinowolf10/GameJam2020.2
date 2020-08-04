a66efd0066e31ef01c85c36a5ad9c7b4eabcd3b6
Rynder1 <liam.ellis37@gmail.com>
2020-08-04 15:21:48 +1000

    Test Scene with artifact
    added stuffs

---
+++ ProjectPuzzle/Assets/Scripts/ArtifactInteract.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class ArtifactInteract : MonoBehaviour
+{
+    public GameObject pickupText;
+    public int artifactId;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            pickupText.SetActive(true);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                Destroy(gameObject);
+                pickupText.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            pickupText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            pickupText.SetActive(false);
+        }
+    }
+
+}
