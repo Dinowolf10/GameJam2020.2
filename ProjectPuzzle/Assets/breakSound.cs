4cf86959f90e1eab807a13180b0ba17fbd697320
Freddi <wesemeyer.fredrik@gmail.com>
2020-08-06 20:24:06 +0200

    destructible sound

---
+++ ProjectPuzzle/Assets/breakSound.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakSound : MonoBehaviour
+{
+    void OnDisable()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Puzzle mechanics/boulderBreak");
+    }
+}
