32e5bad496e7c573e88a66d3ad2d6d52af5a81cd
Rynder1 <liam.ellis37@gmail.com>
2020-08-08 17:45:06 +1000

    Almost done
    almost done

---
+++ ProjectPuzzle/Assets/Scripts/TriggerSceneChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSceneChange : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneController.instance.ChangeScene(sceneName);
+    }
+}
